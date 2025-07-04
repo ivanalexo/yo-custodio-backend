585c08300d13611634c1d5ccdfd5147c45e3b622
Ivan Omonte <ivanalex.oc@gmail.com>
2025-07-03 22:14:21 -0400

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoreModule } from './core/core.module';
+import { GeographicModule } from './modules/geographic/geographic.module';
 
 @Module({
-  imports: [],
+  imports: [CoreModule, GeographicModule],
   controllers: [AppController],
   providers: [AppService],
 })
