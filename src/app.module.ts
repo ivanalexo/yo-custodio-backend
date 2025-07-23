1745121eda2bec07457da1bff96ff98e7e1cf986
Ivan Omonte <ivanalex.oc@gmail.com>
2025-07-23 18:03:44 -0400

    political partied module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { GeographicModule } from './modules/geographic/geographic.module';
+import { PoliticalModule } from './modules/political/political.module';
 
 @Module({
-  imports: [CoreModule, GeographicModule],
+  imports: [CoreModule, GeographicModule, PoliticalModule],
   controllers: [AppController],
   providers: [AppService],
 })
